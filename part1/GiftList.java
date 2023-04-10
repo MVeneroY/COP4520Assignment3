e6dfded86113810ce36b10383e8975fa80f91fd1
MVeneroYupanqui <veneromiguel337@gmail.com>
2023-04-10 19:47:39 -0400

    Added methods

--- part1/GiftList.java
+++ part1/GiftList.java
@@ -1,6 +1,9 @@
 public class GiftList {
 
     GiftNode head = null;
+    // int lastTag = -1;
+    // int nextTag;
+    // boolean started = false;
 
     GiftList() {}
     GiftList(GiftNode head) { this.head = head; }
@@ -12,9 +15,56 @@ public class GiftList {
 
         GiftNode curr = head;
         while (curr != null) {
-            System.out.printf("%d ->", curr.getTag());
+            System.out.printf("%d -> ", curr.getTag());
+            curr = curr.next;
         }
 
         System.out.println("X");
+    }
+
+    public void add(GiftNode node) {
+        if (node == null) return;
+        // started = true;
+
+        if (this.isEmpty()) {
+            this.head = node;
+            node.next = null;
+            node.prev = null;
+            // nextTag = node.getTag();
+            return;
+        }
+
+        GiftNode curr = head;
+        while (curr.next != null) { curr = curr.next; }
+        curr.next = node;
+        node.next = null;
+        node.prev = curr;
+        // nextTag = node.getTag();
+    }
+
+    public int getNextTag() {
+        // if (!started) return 0;
+        // return nextTag;
+        if (this.isEmpty()) return 0;
+        GiftNode curr = head;
+
+        while (curr.next != null) curr = curr.next;
+        return curr.getTag() + 1; 
+    } 
+
+    // removes the head from the list and returns it
+    public GiftNode pop() {
+        if (this.isEmpty()) return null;
+
+        GiftNode curr = this.head;
+        if (curr.next == null) {
+            this.head = null;
+            return curr;
+        }
+
+        this.head = this.head.next;
+        this.head.prev = null;
+
+        return curr;
     }
 }
--- part1/GiftNode.java
+++ part1/GiftNode.java
@@ -6,15 +6,18 @@ public class GiftNode {
         out
     }
     
-    private GiftNode next = null;
-    private GiftNode prev = null;
+    GiftNode next = null;
+    GiftNode prev = null;
+
     private Location location = Location.bag; 
     private int tag;
+    private String card = null;
 
     GiftNode(int tag) { this.tag = tag; }
     public int getTag() { return this.tag; }
     public Location getLocation() { return this.location; }
     public void setLocation(Location location) { this.location = location; }
+    public void setCard(String card) { this.card = card; }
 
     public void link(GiftNode prev) {
         prev.next = this;
