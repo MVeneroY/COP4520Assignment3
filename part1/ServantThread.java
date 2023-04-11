10da6edaa729e20cd1b5fcff60f3bd68e8b050c8
MVeneroYupanqui <veneromiguel337@gmail.com>
2023-04-10 20:19:26 -0400

    Added task 1

--- part1/ServantThread.java
+++ part1/ServantThread.java
@@ -14,11 +14,13 @@ public class ServantThread implements Runnable {
         this.list = list;
     }
 
+    // TODO: implement locks
     @Override
     public void run() {
         switch (task % 3) {
             case 1:
             // Add gift to list
+            addGiftToChain(list);
             break;
             case 2:
             // Write thank you note to gift
@@ -33,6 +35,20 @@ public class ServantThread implements Runnable {
     public void updateTask(int task) { this.task = task; }
 
     // Add to list
+    void addGiftToChain(GiftList list) {
+        GiftNode gift = findNextGift();
+        if (gift != null) list.add(gift);
+    }
+
+    GiftNode findNextGift() {
+        int tag = list.getNextTag();
+        for (int i = 0; i < n_gifts; ++i) {
+            if (gifts[i].getTag() == tag) return gifts[i];
+        }
+
+        return null;
+    }
+
     // Write thank you note
     // Search for gift
 }
