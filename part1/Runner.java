43b350b286391da4e2067159d402715b12858448
MVeneroYupanqui <veneromiguel337@gmail.com>
2023-04-10 20:09:50 -0400

    ServantThread constructor

--- part1/Runner.java
+++ part1/Runner.java
@@ -5,6 +5,8 @@ class Runner {
     static final int n_gifts = 500000;
     static final int n_servants = 4;
     static GiftNode[] gifts = new GiftNode[n_gifts];
+    static ServantThread[] servants = new ServantThread[n_servants];
+    static int[] servant_tasks = new int[n_servants];
     
     static Random rand = new Random();
 
@@ -15,12 +17,29 @@ class Runner {
         for (int i = 0; i < n_gifts; ++i) swap(i, rand.nextInt(n_gifts));
         GiftList list = new GiftList();
         
+        // Initialize servants
+        for (int i = 0; i < n_servants; ++i) {
+            servants[i] = new ServantThread(0, n_gifts, n_servants, gifts, list);
+            servant_tasks[i] = 0;
+        }
+
         // Will continue working in a bit
+        while (!allOut()) {
+            // servants carry out tasks
+        }
     }
 
     public static void swap(int index1, int index2) {
         GiftNode temp = gifts[index1];
         gifts[index1] = gifts[index2];
         gifts[index2] = temp;
+    }
+
+    public static boolean allOut() {
+        for (int i = 0; i < n_gifts; ++i) {
+            if (gifts[i].getLocation() != GiftNode.Location.out) return false;
+        }
+
+        return true;
     }
 }
--- part1/ServantThread.java
+++ part1/ServantThread.java
@@ -1,8 +1,38 @@
 public class ServantThread implements Runnable {
 
+    private int task;
+    int n_gifts;
+    int n_servants;
+    GiftNode[] gifts;
+    GiftList list;
+
+    ServantThread(int task, int n_gifts, int n_servants, GiftNode[] gifts, GiftList list) {
+        this.task = task;
+        this.n_gifts = n_gifts;
+        this.n_servants = n_servants;
+        this.gifts = gifts;
+        this.list = list;
+    }
+
     @Override
     public void run() {
-        
+        switch (task % 3) {
+            case 1:
+            // Add gift to list
+            break;
+            case 2:
+            // Write thank you note to gift
+            break;
+            case 3:
+            // Search for gift in list
+            break;
+            default:
+        }
     }
     
+    public void updateTask(int task) { this.task = task; }
+
+    // Add to list
+    // Write thank you note
+    // Search for gift
 }
